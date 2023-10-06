7b32357c77bac7e40cce44e7412e5d00eab24772
harishbaranwal <114431658+harishbaranwal@users.noreply.github.com>
2023-10-06 23:48:53 +0530

    Create lecture6II.cpp

---
+++ lecture6II.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // the working of pre and post increment.
+
+    // int i;
+    // i = 0;
+    // // cout << i++ << endl;
+    // // cout << i << endl;
+    // // cout << ++i << endl;
+
+    // int j=0 , k=0;
+
+    // cout << i << " " << j << " " << k << endl;
+    // j = i++;
+    // cout << i << " " << j << " " << k << endl;
+    // k = ++i;
+    // cout << i << " " << j << " " << k << endl;
+
+
+
+    // printing numbers from 1 to 100 using for loop.
+
+    for(int i=1;i<=100;i++){
+        // block gets executed everytime.
+        cout << i <<" ";
+    }
+
+    return 0;
+}
